5f2d211a240fdaf3c773f5a94c3e1c4db34f6dc3
Raphael Adam <raphael.adam@webfilings.com>
2014-03-17 14:26:30 -0500

    Comments for show_list function

--- class_exam/shopping_list.py
+++ class_exam/shopping_list.py
@@ -3,18 +3,35 @@ class ShoppingList():
         self.items = items
 
     def show_list(self):
+        """
+        @summary: Prints ShoppingList with borders and space formatting. The
+        width of list is determined by the longest item in the list.
+        Example:
+        My Shopping List:       My Shopping List:
+        -------------           --------------------
+        |  Apples   |           |      Apples      |
+        |  Bananas  |           |     Bananas      |
+        |Watermelons|           |Oranges -Purchased|
+        -------------           |   Watermelons    |
+                                --------------------
+        """
+        # Assign the length of the longest item on the list to list_width
         list_width = max(len(s) for s in self.items)
         list_divider = "-" * (list_width + 2)
         print "My Shopping List:"
         print list_divider
-        for item in self.items:
-            item_len = len(item)
+        # Loop through the items in the list and assign the length to item_len
+        # Find the difference between list_width and item_len to determine the
+        # number of spaces that you need to put around the item.
+        for item in self.items:  # loop through items
+            item_len = len(item)  # assign item_len to the length of each item
             space_diff = list_width - item_len
-            spacing_before = " " * (space_diff/2)
-            if (space_diff % 2) == 0:
-                spacing_after = spacing_before
-            else:
-                spacing_after = " " * ((space_diff/2) + 1)
+            spacing_before = spacing_after = " " * (space_diff/2)
+            # Create an if statement that determines if space_diff is odd.
+            # Change spacing_after to the appropriate number of spaces to keep
+            # the list borders lined up when space_diff is odd.
+            if (space_diff % 2) != 0:  # check if space_diff is odd
+                spacing_after += " "  # change spacing_after
             print "|" + spacing_before + item + spacing_after + "|"
         print list_divider + "\n"
 
