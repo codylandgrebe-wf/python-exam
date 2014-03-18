1544e1fe9e45f4b76c6cbd7e1ddf162cd5d32b53
Raphael Adam <raphael.adam@webfilings.com>
2014-03-18 09:37:28 -0500

    More details

--- class_exam/shopping_list.py
+++ class_exam/shopping_list.py
@@ -16,6 +16,7 @@ class ShoppingList():
                                 --------------------
         """
         # Assign the length of the longest item on the list to list_width
+        #__Write your code below__
         list_width = max(len(s) for s in self.items)
         list_divider = "-" * (list_width + 2)
         print "My Shopping List:"
@@ -23,15 +24,18 @@ class ShoppingList():
         # Loop through the items in the list and assign the length to item_len
         # Find the difference between list_width and item_len to determine the
         # number of spaces that you need to put around the item.
-        for item in self.items:  # loop through items
-            item_len = len(item)  # assign item_len to the length of each item
+        for item in self.items:
+            # Assign item_len to the length of each item
+            #__Write your code below__
+            item_len = len(item)
             space_diff = list_width - item_len
             spacing_before = spacing_after = " " * (space_diff/2)
             # Create an if statement that determines if space_diff is odd.
             # Change spacing_after to the appropriate number of spaces to keep
             # the list borders lined up when space_diff is odd.
-            if (space_diff % 2) != 0:  # check if space_diff is odd
-                spacing_after += " "  # change spacing_after
+            #__Write your code below__
+            if (space_diff % 2) != 0:
+                spacing_after += " "
             print "|" + spacing_before + item + spacing_after + "|"
         print list_divider + "\n"
 
